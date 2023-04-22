985bddf7c5dac84169cabdecb10199fd4c1f3099
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2023-04-23 00:53:00 +0900

    Vue - v-bind, v-for => data 내부 데이터 -> LIST 화 시킨후 <a/> 로 출력

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -2,11 +2,28 @@
   <div>
     <h1>Home page</h1>
   </div>
+
+  <div id="body home">
+      <ul id="locations">
+        <li v-for="list in practiceList" v-bind:key="list">
+          <a v-bind:href="list.ahref">{{list.name}}</a>
+        </li>
+      </ul>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "HomePage"
+  name: "HomePage",
+
+  data() {
+    return {
+        practiceList: [
+          {name: 'AlonePage', ahref: '/alone'},
+          {name: 'DataBindingPage', ahref: '/data'}
+        ]
+    }
+  },//data
 }
 </script>
 
