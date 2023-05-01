7612cbdc4465449daafb7d9f31d79174143c87ba
Seok Jun Kang(Lucas Kang) <swings134@gmail.com>
2023-05-01 20:32:06 +0900

    Vue Make Modal -> ADD LIB(stylus{Css-Loader})

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,22 +1,40 @@
 <template>
-  <div>
-    <h1>Home page</h1>
-  </div>
+  <div class="example-modal-window">
+    <div>
+      <h1>Home page</h1>
+    </div>
 
-  <div id="body home">
-      <ul id="locations">
-        <li v-for="list in practiceList" v-bind:key="list">
-          <a v-bind:href="list.ahref" >{{list.name}}</a>
-        </li>
-      </ul>
-    <button @click="clickList">Modal 창 이동</button>
+    <div id="body home">
+        <ul id="locations">
+          <li v-for="list in practiceList" v-bind:key="list">
+            <a v-bind:href="list.ahref" >{{list.name}}</a>
+          </li>
+        </ul>
+  <!--    <button @click="clickList">Modal 창 이동</button>-->
+      <button @click="openModal">Modal 창 이동</button>
+    </div>
+
+    <!-- Modal -->
+    <div>
+        <TestModal @close="closeModal" v-if="modal">
+            <p>Default Modal Tag</p>
+            <div><input v-model="message"/></div>
+
+            <!--   footer Slot Content     -->
+            <template v-slot:footer>
+              <button @click="sendMsg">메시지 제출</button>
+            </template>
+        </TestModal>
+    </div>
   </div>
 </template>
 
 <script>
+import TestModal from "@/components/modal/TestModal";
 
 export default {
-  name: "HomePage",
+  // name: "HomePage",
+  components: {TestModal},
 
   data() {
     return {
@@ -25,13 +43,31 @@ export default {
           {name: 'DataBindingPage', ahref: '/data'},
           // {name: '팝업창 열기!', ahref: '/modal/:id'}
         ],
+      modal: false,
+      message: '',
+
     }
   },//data
   methods: {
     clickList() {
       //Modal 창으로 설계 했으나, 현재는 페이지 이동 -> with 파라미터
         this.$router.push('/modal/'+'test');
-    }
+    },
+    openModal() {
+      this.modal = true;
+    },
+    closeModal() {
+      this.modal = false;
+    },
+    sendMsg() {
+      if(this.message.length > 0) {
+        alert(this.message);
+        this.message = '';
+        this.closeModal();
+      }else {
+        alert('메시지가 존재하지 않습니다.');
+      }
+    },
 
   },//mehtods
 
